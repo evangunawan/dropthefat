03a51d341a6a384875031e46d411295335bbac7e
motionfrog <evangunawan14@gmail.com>
2020-03-12 19:45:33 +0700

    Add App Bar and Drawer

---
+++ src/components/Container.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Box } from '@material-ui/core';
+
+interface Props {
+  children: React.ReactNode;
+  width?: string;
+}
+
+const Container = (props: Props) => {
+  const containerStyle = {
+    padding: '32px 16px',
+    width: props.width || '100%',
+    margin: '0 auto',
+  };
+
+  return <Box style={containerStyle}>{props.children}</Box>;
+};
+
+export default Container;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import Container from '../components/Container';
+import { Box } from '@material-ui/core';
+import MainAppBar from '../components/MainAppBar';
+
+const MenuButton = () => {
+  return <Box></Box>;
+};
 
 const Home = () => {
   return (
-    <div className={`container`}>
-      <span>Home Page</span>
+    <div>
+      <MainAppBar />
+      <Container>
+        <Container></Container>
+      </Container>
     </div>
   );
 };
