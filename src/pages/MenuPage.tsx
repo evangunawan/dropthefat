3428c1da9717f1a3570c0a6bf924dd20f7eb9dbe
jacobuscalvin <jacobus.calvin@gmail.com>
2020-03-17 21:01:22 +0700

    Add MenuPage and connected with Database

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -1,20 +1,64 @@
 import * as React from 'react';
+import firebase from 'firebase';
+import '@firebase/firestore';
+
 import {
   Button,
   TableContainer,
   Table,
+  TableBody,
   TableHead,
   TableRow,
   TableCell,
 } from '@material-ui/core';
 import Container from '../components/Container';
 
-export class MenuPage extends React.Component {
+type MenuState = { listMenu: any[]; ready: boolean };
+
+export class MenuPage extends React.Component<{}, MenuState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      ready: false,
+      listMenu: [],
+    };
+  }
+
   componentDidMount() {
-    console.log('Hello');
+    this.getMenuList();
+  }
+
+  async getMenuList() {
+    const db = firebase.firestore();
+    let result = null;
+    await db
+      .collection('Menu')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const temp = [...this.state.listMenu, doc.data()];
+          this.setState({ listMenu: temp });
+        });
+      });
+
+    this.setState({ ready: true });
   }
 
   render() {
+    if (!this.state.ready) {
+      return <p>Loading</p>;
+    }
+
+    const items = this.state.listMenu.map((item, key) => {
+      return (
+        <TableRow key={key}>
+          <TableCell align='left'>{item.name}</TableCell>
+          <TableCell align='left'>{item.type}</TableCell>
+          <TableCell align='left'>{item.price}</TableCell>
+        </TableRow>
+      );
+    });
+
     return (
       <Container>
         <h1>Food Menus</h1>
@@ -23,8 +67,11 @@ export class MenuPage extends React.Component {
             <TableHead>
               <TableRow>
                 <TableCell>Food Name</TableCell>
+                <TableCell>Food Type</TableCell>
+                <TableCell>Price</TableCell>
               </TableRow>
             </TableHead>
+            <TableBody>{items}</TableBody>
           </Table>
         </TableContainer>
       </Container>
