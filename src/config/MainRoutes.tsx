4031f170bfe2bf022ee724b4a36147a28ce5d160
jacobuscalvin <jacobus.calvin@gmail.com>
2020-03-17 22:10:42 +0700

    Create LoginAdmin Page

--- src/config/MainRoutes.tsx
+++ src/config/MainRoutes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import OrderPage from '../pages/OrderPage';
 import { MenuPage } from '../pages/MenuPage';
+import LoginAdmin from '../pages/Admin/LoginAdmin';
 
 export default class MainRoutes extends React.Component {
   render() {
@@ -12,6 +13,7 @@ export default class MainRoutes extends React.Component {
           <Route exact path='/' component={Home} />
           <Route exact path='/order' component={OrderPage} />
           <Route exact path='/menu' component={MenuPage} />
+          <Route exact path='/admin' component={LoginAdmin} />
         </Switch>
       </div>
     );
---
+++ src/pages/Admin/LoginAdmin.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Container from '../../components/Container';
+import { CSSProperties } from '@material-ui/core/styles/withStyles';
+import {
+  Card,
+  TextField,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core';
+
+const LoginAdmin = () => {
+  const containerStyle: CSSProperties = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  };
+  const cardStyle: CSSProperties = {
+    width: '500px',
+    height: 'auto',
+  };
+
+  return (
+    <Container style={containerStyle}>
+      <Card style={cardStyle}>
+        <CardContent>
+          <Typography variant='h5' component='h2' align='center'>
+            Login Admin
+          </Typography>
+          <form style={{ padding: 10 }}>
+            <TextField label='Password' variant='outlined' />
+          </form>
+        </CardContent>
+        <CardActions>
+          <Button size='small'>LOGIN</Button>
+        </CardActions>
+      </Card>
+    </Container>
+  );
+};
+
+export default LoginAdmin;
