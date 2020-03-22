4dc423131ec5049515f965b46f032a2729637afb
motionfrog <evangunawan14@gmail.com>
2020-03-22 18:39:00 +0700

    Application can now write to firestore db. Added Loading Spinner

--- src/pages/Order/CreateOrder.tsx
+++ src/pages/Order/CreateOrder.tsx
@@ -19,6 +19,8 @@ import AddMenuModal from './AddMenuModal';
 import firebase from 'firebase';
 import '@firebase/firestore';
 import { MenuOrder } from '../../models/MenuOrder';
+import { Add } from '@material-ui/icons';
+import FullScreenSpinner from '../../components/FullScreenSpinner';
 
 interface TableProps {
   orders: MenuOrder[];
@@ -27,11 +29,15 @@ interface TableProps {
 }
 
 const MenuTable = (props: TableProps) => {
-  // const [items, setItems] = React.useState<Menu[]>([]);
-
   const placeholderStyle = {
     margin: '0 auto',
     padding: '16px',
+  };
+
+  const renderCurrency = (price: number) => {
+    const regex = new RegExp(/\B(?=(\d{3})+(?!\d))/g);
+    const temp = price.toString().replace(regex, ',');
+    return 'Rp' + temp;
   };
 
   const deleteItem = (item: MenuOrder) => {
@@ -46,13 +52,21 @@ const MenuTable = (props: TableProps) => {
     return item.quantity * item.menu.price;
   };
 
+  const renderGrandTotal = (item: MenuOrder[]) => {
+    let result = 0;
+    item.forEach((item) => {
+      result = result + item.quantity * item.menu.price;
+    });
+    return result;
+  };
+
   const renderMenuItems = (items: MenuOrder[]) => {
     return items.map((item, k) => {
       return (
         <TableRow key={k}>
           <TableCell>{item.menu.name}</TableCell>
           <TableCell>{item.menu.type}</TableCell>
-          <TableCell>{item.menu.price}</TableCell>
+          <TableCell>{renderCurrency(item.menu.price)}</TableCell>
           <TableCell>
             <TextField
               type='number'
@@ -61,7 +75,7 @@ const MenuTable = (props: TableProps) => {
               onChange={(ev) => handleQuantityChange(ev, item)}
             />
           </TableCell>
-          <TableCell>{renderPrice(item)}</TableCell>
+          <TableCell>{renderCurrency(renderPrice(item))}</TableCell>
           <TableCell style={{ width: 60 }}>
             <IconButton
               aria-label='Delete'
@@ -80,7 +94,7 @@ const MenuTable = (props: TableProps) => {
     if (items.length < 1 || items === undefined) {
       return (
         <TableRow style={placeholderStyle}>
-          <TableCell colSpan={5}>
+          <TableCell colSpan={6}>
             <Typography align='center' variant='body2' color='textSecondary'>
               <i>Please add a menu</i>
             </Typography>
@@ -117,16 +131,30 @@ const MenuTable = (props: TableProps) => {
             </TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>{renderTableBody(props.orders)}</TableBody>
+        <TableBody>
+          {renderTableBody(props.orders)}
+          <TableRow style={{ backgroundColor: '#111' }}>
+            <TableCell colSpan={5}>
+              <Typography align='right' variant='body2'>
+                <b>Grand Total</b>
+              </Typography>
+            </TableCell>
+            <TableCell>
+              <b>{renderCurrency(renderGrandTotal(props.orders))}</b>
+            </TableCell>
+          </TableRow>
+        </TableBody>
       </Table>
     </TableContainer>
   );
 };
 
 const CreateOrder = () => {
+  const [pic, setPic] = React.useState('');
   const [menuList, setMenuList] = React.useState<Menu[]>([]); //menuList is all loaded menu from db, which will be shown in AddMenuModal
   const [orders, setOrders] = React.useState<MenuOrder[]>([]); //Orders that added in the table.
   const [modalOpen, setModalOpen] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const db = firebase.firestore();
 
   async function fetchMenu() {
@@ -156,14 +184,32 @@ const CreateOrder = () => {
   }, []);
 
   const formStyle: React.CSSProperties = {
+    width: '100%',
     display: 'flex',
-    flexWrap: 'wrap',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    // flexWrap: 'wrap',
     padding: '16px 0',
   };
 
-  const buttonStyle = {
+  const containerStyle: React.CSSProperties = {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'start',
+    alignItems: 'stretch',
+  };
+
+  const btnGroupStyle: React.CSSProperties = {
     width: '100%',
-    margin: '16px 0px',
+    display: 'flex',
+    justifyContent: 'flex-end',
+  };
+
+  const buttonStyle = {
+    width: '250px',
+    margin: '16px auto',
+    alignSelf: 'center',
   };
 
   const addMenu = () => {
@@ -193,6 +239,31 @@ const CreateOrder = () => {
     }
   };
 
+  const createOrder = async () => {
+    setLoading(true);
+    const mOrders: { menu: string; quantity: number }[] = [];
+    let grandTotal = 0;
+    orders.forEach((item) => {
+      grandTotal = grandTotal + item.menu.price * item.quantity;
+      mOrders.push({
+        menu: item.menu.id || 'undefined',
+        quantity: item.quantity,
+      });
+    });
+
+    try {
+      await db.collection('order').add({
+        time: Date.now(),
+        menuOrders: mOrders,
+        pic: pic || 'undefined',
+        total: grandTotal,
+      });
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const handleQtyChange = (ev: any, item: MenuOrder) => {
     const temp = [...orders];
     const index = temp.indexOf(item);
@@ -206,14 +277,22 @@ const CreateOrder = () => {
   }
 
   return (
-    <Container width='1000px'>
-      <Typography variant='h5'>Create an Order</Typography>
-      <Typography variant='body2' color='textSecondary' component='p'>
-        Create an order entry by selecting menu and data below.
-      </Typography>
-      <div style={formStyle}>
-        <form style={{ width: '100%' }}>
-          <TextField fullWidth label='Person in charge' variant='outlined' />
+    <Container width='1000px' style={containerStyle}>
+      <div style={{ flexGrow: 1 }}>
+        <Typography variant='h5'>Create an Order</Typography>
+        <Typography variant='body2' color='textSecondary' component='p'>
+          Create an order entry by selecting menu and data below.
+        </Typography>
+      </div>
+      <div>
+        <form style={formStyle}>
+          <TextField
+            fullWidth
+            label='Person in charge'
+            variant='outlined'
+            value={pic}
+            onChange={(ev) => setPic(ev.target.value)}
+          />
           <Typography variant='h5' style={{ padding: '16px 0px' }}>
             Menu Order
           </Typography>
@@ -223,15 +302,29 @@ const CreateOrder = () => {
             onQtyChange={(ev, item) => handleQtyChange(ev, item)}
           />
           <Button
-            variant='contained'
-            color='primary'
+            variant='text'
+            color='secondary'
+            startIcon={<Add />}
             disableRipple
             style={buttonStyle}
             onClick={addMenu}
           >
-            ADD MENU
+            <b>ADD MENU</b>
           </Button>
         </form>
+      </div>
+      <div style={btnGroupStyle}>
+        <Button variant='text' color='default' style={{ marginRight: 8 }}>
+          <b>Cancel</b>
+        </Button>
+        <Button
+          variant='contained'
+          color='primary'
+          disabled={orders.length < 1}
+          onClick={createOrder}
+        >
+          <b>Create Order</b>
+        </Button>
       </div>
       <AddMenuModal
         open={modalOpen}
@@ -243,6 +336,7 @@ const CreateOrder = () => {
           addSelectedMenu(item);
         }}
       />
+      <FullScreenSpinner open={loading} />
     </Container>
   );
 };
