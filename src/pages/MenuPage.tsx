8ec59f5b2cf5724491f389bb6191a70d64283535
jacobuscalvin <jacobus.calvin@gmail.com>
2020-03-26 23:57:20 +0700

    Change list menu table

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import firebase from 'firebase';
 import '@firebase/firestore';
-
 import {
   TableContainer,
   Table,
@@ -9,81 +8,116 @@ import {
   TableHead,
   TableRow,
   TableCell,
+  Paper,
+  TableFooter,
+  TablePagination,
+  Typography,
 } from '@material-ui/core';
 import Container from '../components/Container';
+import TablePaginationActions from '@material-ui/core/TablePagination/TablePaginationActions';
 
-type MenuState = { listMenu: any[]; ready: boolean };
+// type MenuState = { listMenu: any[]; ready: boolean };
 
-export class MenuPage extends React.Component<{}, MenuState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      ready: false,
-      listMenu: [],
-    };
-  }
+const TableHeader = () => {
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell>Food Name</TableCell>
+        <TableCell>Food Type</TableCell>
+        <TableCell>Price</TableCell>
+      </TableRow>
+    </TableHead>
+  );
+};
 
-  componentDidMount() {
-    this.getMenuList();
-  }
+const MenuPage = () => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [menu, setMenu] = React.useState([] as any[]);
 
-  async getMenuList() {
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const getMenuList = async () => {
     const db = firebase.firestore();
-    let result = null;
+    const result: any[] = [];
     await db
       .collection('menu')
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          const temp = [...this.state.listMenu, doc.data()];
-          this.setState({ listMenu: temp });
+          result.push(doc.data());
+          console.log(doc.data());
         });
       });
+    setMenu(result);
+  };
 
-    this.setState({ ready: true });
-  }
+  const renderMenuType = (val: string) => {
+    if (val === 'drink') return 'Drink';
+    else if (val === 'main-course') return 'Main Course';
+    else if (val === 'dessert') return 'Dessert';
+  };
 
-  renderItemType(menuType: String) {
-    if (menuType == 'main-course') {
-      return 'Main Course';
-    } else if (menuType == 'drink') {
-      return 'Drink';
-    } else if (menuType == 'dessert') {
-      return 'Dessert';
-    }
-  }
-
-  render() {
-    if (!this.state.ready) {
-      return <p>Loading</p>;
-    }
-
-    const items = this.state.listMenu.map((item, key) => {
+  const renderTableBody = (items: any[]) => {
+    const result = items.map((item: any, k) => {
       return (
-        <TableRow key={key}>
-          <TableCell align='left'>{item.name}</TableCell>
-          <TableCell align='left'>{this.renderItemType(item.type)}</TableCell>
-          <TableCell align='left'>{item.price}</TableCell>
+        <TableRow key={k}>
+          <TableCell>{item.name}</TableCell>
+          <TableCell>{renderMenuType(item.type)}</TableCell>
+          <TableCell>{item.price}</TableCell>
         </TableRow>
       );
     });
 
-    return (
-      <Container>
-        <h1>Food Menus</h1>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Food Name</TableCell>
-                <TableCell>Food Type</TableCell>
-                <TableCell>Price</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>{items}</TableBody>
-          </Table>
-        </TableContainer>
-      </Container>
-    );
-  }
-}
+    return result;
+  };
+
+  React.useEffect(() => {
+    getMenuList();
+  }, []);
+
+  return (
+    <Container width='1000px'>
+      <Typography variant='h4' component='h4' align='center' style={{ marginBottom: 12 }}>
+        Food Menu
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHeader />
+          <TableBody>{renderTableBody(menu)}</TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[10, 25, { label: 'All', value: -1 }]}
+                colSpan={3}
+                count={menu.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { 'aria-label': 'rows per page' },
+                  native: true,
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+};
+
+export default MenuPage;
