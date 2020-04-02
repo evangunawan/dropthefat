6969931435718ce5b474ce1062e9305021bb0d7b
georgius kevin <41147450+gkevin9@users.noreply.github.com>
2020-04-02 16:39:06 +0700

    nyontek dari menu page. db masih beda

--- src/pages/Expenditure/ManageExpenditure.tsx
+++ src/pages/Expenditure/ManageExpenditure.tsx
@@ -1,53 +1,91 @@
-import React, { Component } from 'react'
+import * as React from 'react';
+import firebase from 'firebase';
+import '@firebase/firestore';
 
-class Table extends Component {
-   constructor(props: any) {
-      super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
-      this.state = { //state is by default an object
-         students: [
-            { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
-            { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
-            { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
-            { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
-         ]
-      }
-   }
+import {
+  TableContainer,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+} from '@material-ui/core';
+import Container from '../../components/Container';
 
-   renderTableData() {
-      return this.state.students.map((student, index) => {
-         const { id, name, age, email } = student //destructuring
-         return (
-            <tr key={id}>
-               <td>{id}</td>
-               <td>{name}</td>
-               <td>{age}</td>
-               <td>{email}</td>
-            </tr>
-         )
-      })
-   }
+type MenuState = { listVendor: any[]; ready: boolean };
 
-   renderTableHeader() {
-      let header = Object.keys(this.state.students[0])
-      return header.map((key, index) => {
-         return <th key={index}>{key.toUpperCase()}</th>
-      })
-   }
+export class ManageExpenditure extends React.Component<{}, MenuState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      ready: false,
+      listVendor: [],
+    };
+  }
 
+  componentDidMount() {
+    this.getVendorList();
+  }
 
-   render() { //Whenever our class runs, render method will be called automatically, it may have already defined in the constructor behind the scene.
+  async getVendorList() {
+    const db = firebase.firestore();
+    let result = null;
+    await db
+      .collection('menu')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const temp = [...this.state.listVendor, doc.data()];
+          this.setState({ listVendor: temp });
+        });
+      });
+
+    this.setState({ ready: true });
+  }
+
+  renderItemType(menuType: String) {
+    if (menuType == 'main-course') {
+      return 'Main Course';
+    } else if (menuType == 'drink') {
+      return 'Drink';
+    } else if (menuType == 'dessert') {
+      return 'Dessert';
+    }
+  }
+
+  render() {
+    if (!this.state.ready) {
+      return <p>Loading</p>;
+    }
+
+    const items = this.state.listVendor.map((item, key) => {
       return (
-         <div>
-            <h1 id='title'>React Dynamic Table</h1>
-            <table id='students'>
-               <tbody>
-                  <tr>{this.renderTableHeader()}</tr>
-                  {this.renderTableData()}
-               </tbody>
-            </table>
-         </div>
-      )
-   }
+        <TableRow key={key}>
+          <TableCell align='left'>{item.name}</TableCell>
+          <TableCell align='left'>{this.renderItemType(item.type)}</TableCell>
+          <TableCell align='left'>{item.price}</TableCell>
+        </TableRow>
+      );
+    });
+
+    return (
+      <Container>
+        <h1>Food Menus</h1>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Food Name</TableCell>
+                <TableCell>Food Type</TableCell>
+                <TableCell>Price</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{items}</TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
+    );
+  }
 }
 
-export default Table
+export default ManageExpenditure;
