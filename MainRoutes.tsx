60809459acbdd78456e7d3996a7eded036e15138
adriank24 <41040039+adriank24@users.noreply.github.com>
2020-04-02 20:58:53 +0700

    Add files via upload
    Generate page Employee dan Payment (Masih Kosong) dan link nya

---
+++ MainRoutes.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../pages/Home';
+import OrderPage from '../pages/Order/OrderPage';
+import CreateOrder from '../pages/Order/CreateOrder';
+import MenuPage from '../pages/MenuPage';
+import Employee from '../pages/Employee';
+import Payment from '../pages/Payment';
+
+export default class MainRoutes extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/order' component={OrderPage} />
+          <Route exact path='/menu' component={MenuPage} />
+          <Route exact path='/order/create' component={CreateOrder} />
+          <Route exact path='/employee' component={Employee}/>
+          <Route exact path='/payment' component={Payment}/>
+        </Switch>
+      </div>
+    );
+  }
+}
---
+++ Payment.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import firebase from 'firebase';
+import '@firebase/firestore';
+import {
+  TableContainer,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  Paper,
+  TableFooter,
+  TablePagination,
+  Typography,
+  withStyles,
+  TextField,
+} from '@material-ui/core';
+import Container from '../components/Container';
+import TablePaginationActions from '@material-ui/core/TablePagination/TablePaginationActions';
+
+function Payment(){
+    return(
+        <div>
+        <h1>Employee</h1>
+        </div>
+    );
+
+};
+
+export default Payment
