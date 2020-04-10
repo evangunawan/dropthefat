3bb103fa4995cc430293ff08ba51a3fe8a8e8953
Evan Gunawan <evangunawan@live.com>
2020-04-11 01:43:15 +0700

    Add favicon, document title, and production props

--- server.js
+++ server.js
@@ -16,4 +16,8 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
+
+
+app.listen(port, ()=>{
+  console.log('App is running on port ' + port);
+});
