028180ca6bf25793f5f2872907fbff307187bf74
Evan Gunawan <evangunawan@live.com>
2020-04-27 20:12:29 +0700

    Fixed warnings

--- src/models/Expenditure.ts
+++ src/models/Expenditure.ts
@@ -2,9 +2,9 @@ import { Vendor } from './Vendor';
 import { MenuOrder } from './MenuOrder';
 
 export interface Expenditure {
- 	time: number;
-	menuOrders: MenuOrder[];
-    pic: string;
-    vendorName: string;
-    total: number
+  time: number;
+  menuOrders: MenuOrder[];
+  pic: string;
+  vendorName: string;
+  total: number;
 }
