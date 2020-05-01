7d155aaf05ddb7a2498a6324e75157b967e0b17e
Evan Gunawan <evangunawan@live.com>
2020-05-01 13:44:18 +0700

    Fix warning, and minor error

--- src/components/MainRoutes.tsx
+++ src/components/MainRoutes.tsx
@@ -2,28 +2,11 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Home from '../pages/Home';
-import LoginAdmin from '../pages/Admin/LoginAdmin';
-import AdminDashboard from '../pages/Admin/AdminDashboard';
 import OrderPage from '../pages/Order/OrderPage';
 import CreateOrder from '../pages/Order/CreateOrder';
 import ReservationPage from '../pages/Reservation/ReservationPage';
 import CreateReservation from '../pages/Reservation/CreateReservation';
 import MenuPage from '../pages/MenuPage';
-import TableManagement from '../pages/Admin/Tables/TableManagement';
-import MenuManagement from '../pages/Admin/Menu/MenuManagement';
-import CreateMenu from '../pages/Admin/Menu/CreateMenu';
-import UpdateMenu from '../pages/Admin/Menu/UpdateMenu';
-import VendorManagement from '../pages/Admin/Vendor/VendorManagement';
-import UpdateVendor from '../pages/Admin/Vendor/UpdateVendor';
-import CreateVendor from '../pages/Admin/Vendor/CreateVendor';
-import PromoManagement from '../pages/Admin/Promo/PromoManagement';
-import CreatePromo from '../pages/Admin/Promo/CreatePromo';
-import UpdatePromo from '../pages/Admin/Promo/UpdatePromo';
-import EmployeeManagement from '../pages/Admin/Employee/EmployeeManagement';
-import CreateEmployee from '../pages/Admin/Employee/CreateEmployee';
-import UpdateEmployee from '../pages/Admin/Employee/UpdateEmployee';
-import Expenditure from '../pages/Admin/Purchasing/Expenditure';
-import BuyIngredients from '../pages/Admin/Purchasing/BuyIngredients';
 import AdminRoutes from './AdminRoutes';
 
 export default class MainRoutes extends React.Component {
